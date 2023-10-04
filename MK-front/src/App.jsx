93df89fb59007f5d6f5b59f35b5de656a0b14106
kardzae <emmanuelpp21@gmail.com>
2023-10-04 09:58:33 -0500

    actualizando avances

---
+++ MK-front/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Reinoselector from './Componentes/ReinoSelector'
+
+function App() {
+
+  return (
+    <div className="App">
+    <Reinoselector />
+  </div>
+  )
+}
+
+export default App
