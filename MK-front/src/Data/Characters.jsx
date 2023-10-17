c3fb095ccdafd3bbdc82120214468d60ec13c6cb
Hernan <herjosuee@hotmail.com>
2023-10-16 19:10:48 -0500

    Actualizando avances y poniendo vista o modulo invasion

---
+++ MK-front/src/Data/Characters.jsx
@@ -0,0 +1,268 @@
+export const characters = [
+// CAMPEONES OUTWORLD
+//------------------------------------------------------------------------------------------
+  {
+    id: 1,
+    name: "Kotal Kahn", // Nombre del personaje
+    image:"Kotal Kahn.gif", // Imagen del personaje
+    realm: "Outworld", // Reino del personaje
+    strength: 1, // Fuerza del personaje
+    defense: 1 // Defensa del personaje
+  },
+  {
+    id: 2,
+    name: "Sindel",
+    image:"sindel.gif",
+    realm: "Outworld",
+    strength: 1, 
+    defense: 1 
+  },
+  {
+    id: 3,
+    name: "Ermac",
+    image:"ermac.gif",
+    realm: "Outworld",
+    strength: 1, 
+    defense: 1
+  },
+  {
+    id: 4,
+    name: "Reptile",
+    image:"reptile.gif",
+    realm: "Outworld",
+    strength: 1, 
+    defense: 1 
+  },
+  {
+    id: 5,
+    name: "Kitana",
+    image:"Kitana.gif",
+    realm: "Outworld",
+    strength: 1, 
+    defense: 1 
+  },
+  {
+    id: 6,
+    name: "Jade",
+    image:"jade.gif",
+    realm: "Outworld",
+    strength: 1, 
+    defense: 1 
+  },
+  {
+    id: 7,
+    name: "Mileena",
+    image:"meleena.gif",
+    realm: "Outworld",
+    strength: 500, 
+    defense: 500 
+  },
+  {
+    id: 8,
+    name: "Baraka",
+    image:"baraka.gif",
+    realm: "Outworld",
+    strength: 1, 
+    defense: 1 
+  },
+  {
+    id: 9,
+    name: "Sheva",
+    image:"Sheeva.gif",
+    realm: "Outworld",
+    strength: 1, 
+    defense: 1 
+  },
+  {
+    id: 10,
+    name: "Shao kang",
+    image:"shao_kahn.gif",
+    realm: "Outworld",
+    strength: 1, 
+    defense: 1 
+  },
+  {
+    id: 11,
+    name: "Goro",
+    image:"Goro.gif",
+    realm: "Outworld",
+    strength: 500, 
+    defense:  500
+  },
+  {
+    id: 12,
+    name: "Erron Black",
+    image:"erron_black.gif",
+    realm: "Outworld",
+    strength: 1, 
+    defense: 1 
+  },
+// CAMPEONES OUTWORLD
+//------------------------------------------------------------------------------------------
+// CAMPEONES EARTREALM
+  {
+    id: 13,
+    name: "Liu Kang",
+    image: "liu-kang.gif",
+    realm: "Earthrealm",
+    strength: 2000, 
+    defense: 1500 
+  },
+  {
+    id: 14,
+    name: "Kung Lao",
+    image: "kung-lao.gif",
+    realm: "Earthrealm",
+    strength: 1, 
+    defense: 1
+  },
+  {
+    id: 15,
+    name: "Johnny Cage",
+    image: "johnny_cage.gif",
+    realm: "Earthrealm",
+    strength: 1500, 
+    defense: 1500 
+  },
+  {
+    id: 16,
+    name: "Sub-Zero",
+    image: "Sub_zero.gif",
+    realm: "Earthrealm",
+    strength: 1500, 
+    defense: 1555 
+  },
+  {
+    id: 17,
+    name: "Sonya Blade",
+    image: "sonya-blade.gif",
+    realm: "Earthrealm",
+    strength: 1, 
+    defense: 1 
+  },
+  {
+    id: 18,
+    name: "Smoke",
+    image: "smoke.gif",
+    realm: "Earthrealm",
+    strength: 1, 
+    defense: 1 
+  },
+  {
+    id: 19,
+    name: "Stryker",
+    image: "stryker.gif",
+    realm: "Earthrealm",
+    strength: 1540, 
+    defense: 1500 
+  },
+  {
+    id: 20,
+    name: "Nightwolf",
+    image: "nightwolf.gif",
+    realm: "Earthrealm",
+    strength: 1, 
+    defense: 1 
+  },
+  {
+    id: 21,
+    name: "Raiden",
+    image: "raiden.gif",
+    realm: "Earthrealm",
+    strength: 100, 
+    defense: 100 
+  },
+  {
+    id: 22,
+    name: "Cassie Cage",
+    image: "cassie-cage.gif",
+    realm: "Earthrealm",
+    strength: 1, 
+    defense: 1 
+  },
+  {
+    id: 23,
+    name: "Jax",
+    image: "jax.gif",
+    realm: "Earthrealm",
+    strength: 1, 
+    defense: 1 
+  },
+  {
+    id: 24,
+    name: "Kenshi",
+    image: "kenshi.gif",
+    realm: "Earthrealm",
+    strength: 1, 
+    defense: 1 
+  },
+  // CAMPEONES EARTREALM
+  //------------------------------------------------------------------------------------------
+  // CAMPEONES NETHERREALM
+  {
+    id: 25,
+    name: "Scorpion",
+    image: "Scorpion.gif",
+    realm: "Netherrealm",
+    strength: 10000, 
+    defense: 10000 
+  },
+  {
+    id: 26,
+    name: "Noob Saibot",
+    image: "Noob-saibot.gif",
+    realm: "Netherrealm",
+    strength: 5500, 
+    defense: 5500 
+  },
+  {
+    id: 27,
+    name: "Quan Chi",
+    image: "quan_chi.gif",
+    realm: "Netherrealm",
+    strength: 5500, 
+    defense: 5100 
+  },
+  {
+    id: 28,
+    name: "Shinnok",
+    image: "Shinnok.gif",
+    realm: "Netherrealm",
+    strength: 5000, 
+    defense: 5000 
+  },
+  {
+    id: 29,
+    name: "Ermac",
+    image: "ermac.gif",
+    realm: "Netherrealm",
+    strength: 110, 
+    defense: 10 
+  },
+  {
+    id: 30,
+    name: "D'Vorah",
+    image: "dvorah.gif",
+    realm: "Netherrealm",
+    strength: 140, 
+    defense: 30 
+  },
+  {
+    id: 31,
+    name: "Ferra/Torr",
+    image: "ferra-orr.gif",
+    realm: "Netherrealm",
+    strength: 190, 
+    defense: 20 
+  },
+  {
+    id: 32,
+    name: "Kano",
+    image: "kano.gif",
+    realm: "Netherrealm",
+    strength: 130, 
+    defense: 60 
+  },
+  // CAMPEONES NETHERREALM
+];
+
---
+++ MK-front/src/Data/Reinos.jsx
@@ -0,0 +1,19 @@
+export const reinos = [
+    {
+      id: 1,
+      name: "Earthrealm",
+      image: "earthrealm.gif"
+    },
+    {
+      id: 2,
+      name: "Outworld",
+      image: "outworld.png"
+    },
+    {
+      id: 3,
+      name: "Netherrealm",
+      image: "netherrealm.gif"
+    },
+    // ...otros reinos
+  ];
+
