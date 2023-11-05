691c9525fc65157312f6a9da914482e424ae6c4f
piterandres <piteryu83@gmail.com>
2023-11-05 15:58:42 -0500

    Historia de reino titanides

---
+++ MK-front/src/Data/Characterslore.jsx
@@ -0,0 +1,25 @@
+export const characterslore = [
+
+    //CAMPEONES TITANIDES
+    {
+     
+      name: "Kronika", // Nombre del personaje
+      image:"Kronika.jpg", // Imagen del personaje
+      realm: "Titanides", // Reino del personaje
+      history:['Es una Titaness y ex Guardiana del Tiempo, madre de los Dioses Ancianos Shinnok y Cetrion, ella es la arquitecta del destino y la diosa del tiempo, y su objetivo es reiniciar la línea de tiempo para crear una nueva era que ella pueda controlar']
+    },
+    {
+      
+      name: "Geras", // Nombre del personaje
+      image:"Geras.jpg", // Imagen del personaje
+      realm: "Titanides", // Reino del personaje
+      history:['Es un constructo creado por Kronika,y se encarga de llevar a cabo su voluntad,Geras tiene la capacidad de manipular el tiempo, lo que utiliza a su favor en combate']
+    },
+    {
+      
+      name: "Cetrion", // Nombre del personaje
+      image:"Cetrion.jpg", // Imagen del personaje
+      realm: "Titanides", // Reino del personaje
+      history:['Cetrion es la hija de kronika,Es una diosa anciana que tiene el control sobre los elementos de la naturaleza, como el fuego, el agua, la tierra y el aire']
+    },
+  ];
